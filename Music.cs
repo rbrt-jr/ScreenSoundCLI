00e6015f2d0b15cc3af32ac42d2f8aa6bde5b79c
Your Name <you@example.com>
2023-08-10 20:44:42 +0100

    first commit

--- Music.cs
+++ Music.cs
@@ -1,16 +1,19 @@
-﻿class Music
+﻿using System.Reflection.Metadata.Ecma335;
+
+class Music
 {
-    public string name;
-    public string artist;
-    public int duration;
-    public bool available;
+    public string Name { get; set; }
+    public string Artist { get; set; }
+    public int Duration { get; set; }
+    public bool Available { get; set; }
+    public string ShortResume => $"The song {Name} belongs to the artist {Artist}";
 
     public void ShowDetails()
     {
-        Console.WriteLine($"Name: {this.name}");
-        Console.WriteLine($"Artist: { this.artist}");
-        Console.WriteLine($"Duration: {this.duration}");
-        if (available)
+        Console.WriteLine($"Name: {this.Name}");
+        Console.WriteLine($"Artist: { this.Artist}");
+        Console.WriteLine($"Duration: {this.Duration}");
+        if (Available)
         {
             Console.WriteLine($"Available: it's available");
         } else
@@ -19,6 +22,9 @@
         }
         
     }
+
+
+   
 }
 
 
--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿Music musicTest1 = new Music();
-musicTest1.name = "New kid in the town";
-musicTest1.artist = "Eagles";
-musicTest1.duration = 263;
-musicTest1.available = true;
+musicTest1.Name = "New kid in the town";
+musicTest1.Artist = "Eagles";
+musicTest1.Duration = 263;
+musicTest1.Available=true;
+
+
 
 Music musicTest2 = new Music();
-musicTest2.name = "All this love";
-musicTest2.artist = "DeBarge";
-musicTest2.duration = 300;
-musicTest2.available = false;
+musicTest2.Name = "All this love";
+musicTest2.Artist = "DeBarge";
+musicTest2.Duration = 300;
+musicTest2.Available = false;
+
 
 musicTest1.ShowDetails();
+Console.WriteLine(musicTest1.ShortResume);
 musicTest2.ShowDetails();
+Console.WriteLine(musicTest2.ShortResume);
 
+Console.WriteLine(musicTest1.Sum(4, 6));
