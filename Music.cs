e5b77695a3a16fd54625e6d51a21c2121660e778
Your Name <you@example.com>
2023-08-10 11:11:26 +0100

    first commit

---
+++ Music.cs
@@ -0,0 +1,25 @@
+﻿class Music
+{
+    public string name;
+    public string artist;
+    public int duration;
+    public bool available;
+
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Name: {this.name}");
+        Console.WriteLine($"Artist: { this.artist}");
+        Console.WriteLine($"Duration: {this.duration}");
+        if (available)
+        {
+            Console.WriteLine($"Available: it's available");
+        } else
+        {
+            Console.WriteLine("$Available: it isn't available");
+        }
+        
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Music musicTest1 = new Music();
+musicTest1.name = "New kid in the town";
+musicTest1.artist = "Eagles";
+musicTest1.duration = 263;
+musicTest1.available = true;
+
+Music musicTest2 = new Music();
+musicTest2.name = "All this love";
+musicTest2.artist = "DeBarge";
+musicTest2.duration = 300;
+musicTest2.available = false;
+
+musicTest1.ShowDetails();
+musicTest2.ShowDetails();
+
